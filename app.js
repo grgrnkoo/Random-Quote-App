8292d24a57644900d580029d8f09dd245dd8145e
Oleg <grigorenko996@gmail.com>
2024-06-17 19:19:35 +0300

    found more reliable fetching solution

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const quote = document.getElementById('quote');
 const idNumber = document.getElementById('id-number');
 const dice = document.getElementById('dice');
 
-const refreshQuote = () => {
-    fetch('https://api.adviceslip.com/advice').then((request) => {
-        return request.json();
-    }).then((data) => {
-        quote.textContent = `"${data.slip.advice}"`;
-        idNumber.textContent = data.slip.id;
-    })
+// const refreshQuote = () => {
+//     fetch('https://api.adviceslip.com/advice').then((request) => {
+//         return request.json();
+//     }).then((data) => {
+//         quote.textContent = `"${data.slip.advice}"`;
+//         idNumber.textContent = data.slip.id;
+//     })
+// }
+
+const refreshQuote = async () => {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const { slip } = await res.json();
+
+    console.log(slip);
+
+    quote.textContent = `"${slip.advice}"`;
+    idNumber.textContent = slip.id;
 }
 
 refreshQuote();
