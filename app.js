1c4ee98f10235c8b5c4848d9ec5acd2cc28409ae
Oleg <grigorenko996@gmail.com>
2024-06-17 18:57:07 +0300

    apply js script

---
+++ app.js
@@ -0,0 +1,15 @@
+const quote = document.getElementById('quote');
+const idNumber = document.getElementById('id-number');
+const dice = document.getElementById('dice');
+
+const refreshQuote = () => {
+    fetch('https://api.adviceslip.com/advice').then((request) => {
+        return request.json();
+    }).then((data) => {
+        quote.textContent = data.slip.advice;
+        idNumber.textContent = data.slip.id;
+    })
+}
+
+refreshQuote();
+dice.addEventListener('click', refreshQuote);
